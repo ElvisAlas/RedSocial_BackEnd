ecde1282e1bb4dfec6a7c4414f9ac89692afcdb8
ElvisAlas <110005376+ElvisAlas@users.noreply.github.com>
2024-03-17 01:16:12 -0600

    subiendo 1er commit

--- Routers/validarUsuario.js
+++ Routers/validarUsuario.js
@@ -3,7 +3,6 @@ import { GetUsuario } from "../Controller/ValidarUsuario.js";
 
 const validarUsuario = express.Router();
 
-// Definir la ruta para validar usuario
 validarUsuario.get('/:Usuario/:Contrasena', GetUsuario);
 
 export { validarUsuario };
--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// import express from 'express';
-
-// import { validarUsuario } from './routers/ValidarUsuario.js';
-// const app = express();
-
-// app.use(express.json());
-
-// const port = 4000;
-
-
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); 
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
-//   next();
-// });
-
-// app.use('/api/validarUsuario',validarUsuario);
-
-// app.listen(port, () => {
-//   console.log(`Escuchando en el puerto ${port}`);
-// });
-
 import express from 'express';
 import { validarUsuario } from './routers/ValidarUsuario.js';
 
